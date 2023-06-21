131f442330f7e3c26c5b98914718216fa01d75ec
Juan Uriarte <uriarte.juan@csu.fullerton.edu>
2023-06-21 11:52:57 -0700

    Hello file not working, doesnt open and no sound is played

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+# Juan Uriarte
+# uriarte.juan@csu.fullerton.edu
+# uriartej
 
 """
 Imports the the game demo and executes the main function.
 """
 
 import sys
+import pygame
 import game
 
 if __name__ == "__main__":
     # TODO: Prepare and run the game
+    pygame.init()
+
+    screen_width = 800
+    screen_height = 800
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("My Game")
+
+
     sys.exit(0)
